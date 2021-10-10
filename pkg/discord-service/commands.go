a58634510b5dcb949d0452a22cfaa37f3bf5c7e9
Stanislaw Krowicki <krowicki00@gmail.com>
2021-10-10 21:21:23 +0200

    Discord bot init and source command

--- pkg/discord-service/commands.go
+++ pkg/discord-service/commands.go
@@ -1,1 +1,16 @@
 package discord_service
+
+import "github.com/bwmarrin/discordgo"
+
+const (
+	Github = "https://github.com/stanislawkrowicki/go-substitutions"
+)
+
+func SourceCode(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	_ = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: Github,
+		},
+	})
+}
--- services/discord-service/discord.go
+++ services/discord-service/discord.go
@@ -1,1 +1,81 @@
-package discord_service
+package main
+
+import (
+	"flag"
+	"github.com/bwmarrin/discordgo"
+	discordService "go-substitutions/pkg/discord-service"
+	"go-substitutions/pkg/env"
+	"log"
+	"os"
+	"os/signal"
+)
+
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "source",
+			Description: "Bot's source code",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"source": discordService.SourceCode,
+	}
+)
+
+const (
+	CantLoadEnv = "Failed to load .env file."
+)
+
+var s *discordgo.Session
+
+func init() { flag.Parse() }
+
+func init() {
+	err := env.LoadEnv()
+	if err != nil {
+		log.Fatal(CantLoadEnv)
+	}
+}
+
+func init() {
+	var err error
+	s, err = discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
+func init() {
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Println("Bot is up!")
+	})
+
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Failed to open connection. %v", err)
+		return
+	}
+
+	for _, v := range commands {
+		_, err := s.ApplicationCommandCreate(s.State.User.ID, os.Getenv("GUILD_ID"), v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+	}
+
+	defer s.Close()
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Println("Shutting down...")
+}
